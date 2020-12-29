b1725def5735cb370577db06fa36e0d75378833d
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2020-12-29 23:10:49 +0530

    Update app.config.js
    rinkerby/fee address

--- app.config.js
+++ app.config.js
@@ -3,12 +3,12 @@ module.exports = {
   // on start. App will automatically switch network configs when user switches
   // networks in their wallet.
   // Ocean Protocol contracts are deployed for: 'mainnet', 'rinkeby', 'development'
-  network: process.env.GATSBY_NETWORK || 'mainnet',
-  infuraProjectId: process.env.GATSBY_INFURA_PROJECT_ID || 'xxx',
+  network: process.env.GATSBY_NETWORK || 'rinkerby',
+  infuraProjectId: process.env.GATSBY_INFURA_PROJECT_ID || '6ba6f1f72b954dc894e9d117fa37e013',
   // The ETH address the marketplace fee will be sent to.
   marketFeeAddress:
     process.env.GATSBY_MARKET_FEE_ADDRESS ||
-    '0x903322C7E45A60d7c8C3EA236c5beA9Af86310c7',
+    '0xc966Ba2a41888B6B4c5273323075B98E27B9F364',
   // Used for conversion display, can be whatever coingecko API supports
   // see: https://api.coingecko.com/api/v3/simple/supported_vs_currencies
   currencies: [
